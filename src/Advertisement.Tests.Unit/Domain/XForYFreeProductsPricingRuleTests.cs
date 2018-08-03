779040ed53f65fe629ee828b38e9856cf5ef66ed
Dalbir Bijral <Dalbir.Bijral@sportsbet.com.au>
2018-08-03 11:44:56 +1000

    missed test

--- src/Advertisement.Tests.Unit/Domain/XForYFreeProductsPricingRuleTests.cs
+++ src/Advertisement.Tests.Unit/Domain/XForYFreeProductsPricingRuleTests.cs
@@ -1,110 +1,87 @@
-﻿//using System.Collections.Generic;
-//using System.Linq;
-//using Advertisement.Domain;
-//using Advertisement.Domain.PricingRules;
-//using AutoFixture;
-//using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Advertisement.Domain;
+using Advertisement.Domain.PricingRules;
+using AutoFixture;
+using NUnit.Framework;
 
-//namespace Advertisement.Tests.Unit.Domain
-//{
-//    [TestFixture]
-//    public class XForYFreeProductsPricingRuleTests
-//    {
-//        [SetUp]
-//        public void SetUp()
-//        {
-//            _testContext = new TestContext();
-//        }
+namespace Advertisement.Tests.Unit.Domain
+{
+    [TestFixture]
+    public class XForYFreeProductsPricingRuleTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _testContext = new TestContext();
+        }
 
-//        private TestContext _testContext;
+        private TestContext _testContext;
 
-//        private class TestContext
-//        {
-//            private IFixture _fixture;
-//            private bool _isApplicable;
-//            private List<Product> _products;
-//            private XForYFreeProducts _sut;
-
-
-//            public void Arrange(string applicableProductId, string productId, int x, int y)
-//            {
-//                _fixture = new Fixture();
-
-//                _fixture.Register(() => new Product(productId, 100.55));
-
-//                _products = _fixture.CreateMany<Product>().ToList();
-
-//                _sut = new XForYFreeProducts(applicableProductId, x, y);
-//            }
-
-//            public void ArrangeRuleIsApplicable()
-//            {
-//                Arrange("classic", "classic", 3, 2);
-//            }
+        private class TestContext
+        {
+            private IFixture _fixture;
+            private bool _isApplicable;
+            private List<Product> _products;
+            private XForYFreeProducts _sut;
 
 
-//            public void ActIsApplicable()
-//            {
-//                _isApplicable = _sut.IsApplicable(_products);
-//            }
+            public void Arrange(string applicableProductId, string productId, int numberOfProducts, int x, int y)
+            {
+                _fixture = new Fixture();
 
-//            public void ActApply()
-//            {
-//                _sut.Apply(_products);
-//            }
+                _fixture.Register(() => new Product(productId, 100.55));
 
-//            public void AssertIsApplicable(bool expectedIsApplicable)
-//            {
-//                Assert.AreEqual(expectedIsApplicable, _isApplicable);
-//            }
+                _products = _fixture.CreateMany<Product>(numberOfProducts).ToList();
 
-//            public void AssertProductPriceIsUpdated()
-//            {
-//                foreach (var product in _products)
-//                {
-//                    Assert.AreEqual(80.55, product.Price);
-//                }
-//            }
+                _sut = new XForYFreeProducts(applicableProductId, x, y);
+            }
 
-//            public void AssertProductPriceIsNotUpdated()
-//            {
-//                foreach (var product in _products)
-//                {
-//                    Assert.AreEqual(100.55, product.Price);
-//                }
-//            }
-//        }
 
-//        [Test]
-//        [TestCase("classic", "classic", ,y,true)]
-//        [TestCase("blah", "classic", false)]
-//        public void Test_is_applicable(string applicableProductId, string productId, int x, int y, bool expected)
-//        {
-//            _testContext.Arrange(applicableProductId, productId, x, y);
+            public void ActIsApplicable()
+            {
+                _isApplicable = _sut.IsApplicable(_products);
+            }
 
-//            _testContext.ActIsApplicable();
+            public void ActApply()
+            {
+                _sut.Apply(_products);
+            }
 
-//            _testContext.AssertIsApplicable(expected);
-//        }
+            public void AssertIsApplicable(bool expectedIsApplicable)
+            {
+                Assert.AreEqual(expectedIsApplicable, _isApplicable);
+            }
 
-//        [Test]
-//        public void Test_after_applying_product_price_is_updated()
-//        {
-//            _testContext.Arrange("classic", "classic");
+            public void AssertProductCountIsCorrect(int expectedNumberOfProducts)
+            {
+                Assert.AreEqual(expectedNumberOfProducts, _products.Count);
+                foreach (var product in _products) Assert.AreEqual(100.55, product.Price);
+            }
+        }
 
-//            _testContext.ActApply();
+        [Test]
+        [TestCase("classic", "classic", 4, 3, 2, true)]
+        [TestCase("classic", "classic", 2, 3, 4, false)]
+        [TestCase("blah", "classic", 4, 3, 2, false)]
+        public void Test_is_applicable(string applicableProductId, string productId, int numberOfProducts, int x,
+            int y, bool expected)
+        {
+            _testContext.Arrange(applicableProductId, productId, numberOfProducts, x, y);
 
-//            _testContext.AssertProductPriceIsUpdated();
-//        }
+            _testContext.ActIsApplicable();
 
-//        [Test]
-//        public void Test_after_applying_product_price_is_not_updated()
-//        {
-//            _testContext.Arrange("classic", "blah");
+            _testContext.AssertIsApplicable(expected);
+        }
 
-//            _testContext.ActApply();
+        [Test]
+        public void Test_when_rule_applies_product_count_is_updated()
+        {
+            _testContext.Arrange("classic", "classic", 3, 3, 2);
 
-//            _testContext.AssertProductPriceIsNotUpdated();
-//        }
-//    }
-//}
+            _testContext.ActApply();
+
+            _testContext.AssertProductCountIsCorrect(2);
+        }
+    }
+}
